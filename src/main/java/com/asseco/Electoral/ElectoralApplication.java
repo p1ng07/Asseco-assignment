11dedf7840911a0e9e7a4723aadfde6844aaa803
p1ng07 <franciscoppontes369@gmail.com>
2024-05-15 23:43:26 +0100

    Initial spring boot application

---
+++ src/main/java/com/asseco/Electoral/ElectoralApplication.java
@@ -0,0 +1,13 @@
+package com.asseco.Electoral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectoralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectoralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asseco/Electoral/ElectoralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asseco.Electoral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectoralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
