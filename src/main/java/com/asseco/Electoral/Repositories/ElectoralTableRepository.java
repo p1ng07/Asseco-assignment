76980de755e43e7e65066095d04826cc9c9a6c63
p1ng07 <franciscoppontes369@gmail.com>
2024-05-19 02:12:51 +0100

    changed(repositories): Added filters to person/table repo

--- src/main/java/com/asseco/Electoral/Repositories/ElectoralTableRepository.java
+++ src/main/java/com/asseco/Electoral/Repositories/ElectoralTableRepository.java
@@ -1,9 +1,17 @@
 package com.asseco.Electoral.Repositories;
 
+import com.asseco.Electoral.Models.District;
 import com.asseco.Electoral.Models.ElectoralTable;
+import com.asseco.Electoral.Models.Municipality;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ElectoralTableRepository extends JpaRepository<ElectoralTable, Long> {
+    List<ElectoralTable> findByDistrictAndMunicipality(District district, Municipality municipality);
+    List<ElectoralTable> findByDistrict(District district);
+    List<ElectoralTable> findByMunicipality(Municipality municipality);
+
+    Optional<ElectoralTable> findByNameIdentifier(String nameIdentifier);
 }
